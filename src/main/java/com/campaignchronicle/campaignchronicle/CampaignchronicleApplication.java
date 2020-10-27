6f97b5f71897ab4053ddae3afd59cea191568b23
joe <jmburghardt@gmail.com>
2020-10-26 22:58:46 -0500

    versions are a bitch; reading from mongo atlas

--- src/main/java/com/campaignchronicle/campaignchronicle/CampaignchronicleApplication.java
+++ src/main/java/com/campaignchronicle/campaignchronicle/CampaignchronicleApplication.java
@@ -1,13 +1,22 @@
 package com.campaignchronicle.campaignchronicle;
 
-import org.springframework.boot.SpringApplication;
+import com.mongodb.client.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+
 public class CampaignchronicleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CampaignchronicleApplication.class, args);
+		String uri = "mongodb+srv://joe123:joe123@ccdata.oa2ck.mongodb.net/test?retryWrites=true&w=majority";
+		try (MongoClient mongoClient = MongoClients.create(uri)){
+			MongoIterable<String> strings = mongoClient.listDatabaseNames();
+			MongoCursor<String> cursor = strings.cursor();
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+		}
+//		SpringApplication.run(CampaignchronicleApplication.class, args);
 	}
 
 }
