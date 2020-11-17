79f86108f7e9c7d98becd5ff61d4a22f1d115e9f
joe <jmburghardt@gmail.com>
2020-11-16 21:39:27 -0600

    updated controller to restcontroller; now returns data as expected

--- src/main/java/com/campaignchronicle/campaignchronicle/controllers/StartingController.java
+++ src/main/java/com/campaignchronicle/campaignchronicle/controllers/StartingController.java
@@ -3,13 +3,13 @@ package com.campaignchronicle.campaignchronicle.controllers;
 import com.campaignchronicle.campaignchronicle.entities.Users;
 import com.campaignchronicle.campaignchronicle.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class StartingController {
 
--- src/main/java/com/campaignchronicle/campaignchronicle/repositories/UserRepository.java
+++ src/main/java/com/campaignchronicle/campaignchronicle/repositories/UserRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<Users, String> {
-    List<Users> findAll();
 }
